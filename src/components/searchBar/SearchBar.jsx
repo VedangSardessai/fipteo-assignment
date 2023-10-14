16493cf17ee1ad32565b1a19196d401b3af094ff
VedangSardessai <vms112000@gmail.com>
2023-10-14 20:31:35 +0530

    ui: searchbar component built

---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./search.css";
+import { BsSearch } from "react-icons/bs";
+export default function SearchBar() {
+  return (
+    <div className="searchBarDiv">
+      <h1 className="userHeader">Hello Shahrukh!</h1>
+      <div className="searchBar">
+        <BsSearch className="searchIcon" />
+
+        <input
+          placeholder="Search"
+          className="search"
+          type="text"
+          name=""
+          id=""
+        />
+      </div>
+    </div>
+  );
+}
