7c901f4863cf3bbd2e199cc1cc405953d6450f79
VedangSardessai <vms112000@gmail.com>
2023-10-17 20:48:56 +0530

    ui: minor css changes and imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import Financials from "./components/financials/Financials";
 import SearchBar from "./components/searchBar/SearchBar";
 import Overview from "./components/overview/Overview";
-
+import Sales from "./components/sales/Sales";
 function App() {
   return (
     <div className="App">
@@ -12,8 +12,8 @@ function App() {
         <Dashboard />
         <div className="bodyContent">
           <Financials />
-
           <Overview />
+          <Sales />{" "}
         </div>
       </div>
     </div>
